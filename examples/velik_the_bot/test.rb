967219057a3ddf92408cd75e9cea69d8efa0e492
Victor Maslov <nakilon@gmail.com>
2022-02-08 03:04:13 +0300

    dry

--- examples/velik_the_bot/test.rb
+++ examples/velik_the_bot/test.rb
@@ -7,13 +7,20 @@ Async = nil
 require "minitest/mock"
 require "minitest/autorun"
 describe "" do
-  it do
-    TrovoBot.stub :start, (lambda do |&b|
-      b.({type: 0, content: "ping"})
-      assert_equal [["pong", nil]], TrovoBot::queue.size.times.map{ TrovoBot::queue.pop }
-    end) do
-      # ARGV[0] = ARGV[1] = ""
-      load "main.rb"
-    end
+
+  def a _in, _out
+    TrovoBot.stub :start, ->&b{
+      b.({type: 0, content: _in})
+      assert_equal [[_out, nil]], TrovoBot::queue.size.times.map{ TrovoBot::queue.pop }
+    } do load "main.rb" end
   end
+
+  it "ping" do
+    a "ping", "pong"
+  end
+
+  it "\\access quote" do
+    a "\\access quote", "'s current access level: 8_owner"
+  end
+
 end
