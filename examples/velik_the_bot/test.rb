8b49f9b4b88186d46aaea23ccb482781d7185924
Victor Maslov <nakilon@gmail.com>
2022-02-08 05:01:15 +0300

    dry

--- examples/velik_the_bot/test.rb
+++ examples/velik_the_bot/test.rb
@@ -9,47 +9,39 @@ require "minitest/mock"
 require "minitest/autorun"
 describe "" do
 
-  def a _out, _in, sender_id = nil
+  def b _in, _out, sender_id = "owner"
     TrovoBot.stub :start, ->&b{
-      b.({type: 0, content: _in, sender_id: sender_id}, "owner")
+      _in.each do |_|
+        b.({type: 0, content: _, sender_id: sender_id}, "owner")
+      end
     } do load "main.rb" end
-    assert_equal [[_out, "owner"]], TrovoBot::queue.size.times.map{ TrovoBot::queue.pop }
+    assert_equal _out.map{ |_| [_, "owner"] }, TrovoBot::queue.size.times.map{ TrovoBot::queue.pop }
   end
 
   it "ping" do
-    a "pong", "ping"
+    b ["ping"], ["pong"]
   end
 
   it "\\access quote get" do
     TrovoBot.stub :name_to_id, ->_{_} do
     TrovoBot.stub :admin_name, "admin" do
     TrovoBot.stub :channel_name, "owner" do
-      a "'s current access level: 8_owner", "\\access quote", "owner"
-      a "'s current access level: 9_admin", "\\access quote", "admin"
-      a "'s current access level: 0_query", "\\access quote", "someone"
-      a "owner's current access level: 8_owner", "\\access quote owner"
-      a "admin's current access level: 9_admin", "\\access quote admin"
-      a "someone's current access level: 0_query", "\\access quote someone"
+      b ["\\access quote"], ["'s current access level: 8_owner"], "owner"
+      b ["\\access quote"], ["'s current access level: 9_admin"], "admin"
+      b ["\\access quote"], ["'s current access level: 0_query"], "someone"
+      b ["\\access quote owner"], ["owner's current access level: 8_owner"]
+      b ["\\access quote admin"], ["admin's current access level: 9_admin"]
+      b ["\\access quote someone"], ["someone's current access level: 0_query"]
     end
     end
     end
   end
   it "\\access quote set" do
-    b = lambda do |_in, _out|
-      TrovoBot.stub :start, ->&b{
-        _in.each do |_|
-          b.({type: 0, content: _, sender_id: "owner"}, "owner")
-        end
-      } do
-        File.write "db.yaml", YAML.dump({})
-        load "main.rb"
-      end
-      assert_equal _out.map{ |_| [_, "owner"] }, TrovoBot::queue.size.times.map{ TrovoBot::queue.pop }
-    end
     TrovoBot.stub :name_to_id, ->_{_} do
     TrovoBot.stub :admin_name, "admin" do
     TrovoBot.stub :channel_name, "owner" do
-      b.call [
+      File.write "db.yaml", YAML.dump({})
+      b [
         "\\access quote someone",
         "\\access quote someone +",
         "\\access quote someone",
