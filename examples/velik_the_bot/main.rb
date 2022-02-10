64aa299ea662634289cb1feb1297e1d578ba16b6
Victor Maslov <nakilon@gmail.com>
2022-02-10 07:26:08 +0300

    cmd aliases

--- examples/velik_the_bot/main.rb
+++ examples/velik_the_bot/main.rb
@@ -4,9 +4,17 @@ require "trovobot"
 
 require "yaml/store"
 db = YAML::Store.new "db.yaml"
+
+resolve = lambda do |cmd|
+  {
+    "q"=>"quote", "quote"=>"quote", "цитата"=>"quote",
+    "b"=>"bet", "bet"=>"bet", "ставка"=>"bet",
+  }.fetch cmd
+end
 get_level = lambda do |sender_id, channel_id, cmd, tr = nil|
   next "8 (owner)" if sender_id == channel_id
   next "9 (admin)" if sender_id == TrovoBot.name_to_id(TrovoBot.admin_name)
+  cmd = resolve[cmd]
   level = if tr
                                tr.fetch "access.#{cmd}.#{channel_id}.#{sender_id}", 0
   else
@@ -17,6 +25,11 @@ get_level = lambda do |sender_id, channel_id, cmd, tr = nil|
     "bet" => %w{ participate initiate },
   }.fetch(cmd).fetch(level)})"
 end
+
+re_help = "(?:\\?|help|хелп|помощь|справка)"
+re_access = "(?:a|access|доступ|права)"
+re_quote = "(?:q|quote|цитата)"
+re_bet = "(?:b|bet|ставка)"
 
 TrovoBot.start do |chat, channel_id|   # this is designed for a multichannel bot it's yet a singleton
   # case msg[:type]
@@ -30,25 +43,25 @@ TrovoBot.start do |chat, channel_id|   # this is designed for a multichannel bot
         next TrovoBot::queue.push [chat[:content].tr("иИ", "оO"), channel_id] if "пинг" == chat[:content].downcase
         case chat[:content].strip
 
-        when /\A\\help\s+access(\s|\z)/
+        when /\A\\#{re_help}\s+#{re_access}(\s|\z)/
           TrovoBot::queue.push [
             "see own access level: \\access {quote,bet}; "\
             "see someone's access level: \\access {quote,bet} <nickname>; "\
             "add/remove ability to add new quotes or initiate bets (only for channel owner and bot admin): \\access {quote,bet} <nickname> +/-",
             channel_id
           ]
-        when /\A\\access\s+(quote|bet)\z/
-          TrovoBot::queue.push ["#{chat[:nick_name]}'s current access level: #{get_level[chat[:sender_id], channel_id, $1]}", channel_id]
-        when /\A\\access\s+(quote|bet)\s+(\S+)\z/
-          TrovoBot::queue.push ["#{$2}'s current access level: #{get_level[TrovoBot::name_to_id($2), channel_id, $1]}", channel_id]
-        when /\A\\access\s+(quote|bet)\s+(\S+)\s+([+-])\z/
+        when /\A\\#{re_access}\s+(#{re_quote}|#{re_bet})\z/
+          TrovoBot::queue.push ["#{chat[:nick_name]}'s current \\#{$1} access level: #{get_level[chat[:sender_id], channel_id, $1]}", channel_id]
+        when /\A\\#{re_access}\s+(#{re_quote}|#{re_bet})\s+(\S+)\z/
+          TrovoBot::queue.push ["#{$2}'s current \\#{$1} access level: #{get_level[TrovoBot::name_to_id($2), channel_id, $1]}", channel_id]
+        when /\A\\#{re_access}\s+(#{re_quote}|#{re_bet})\s+(\S+)\s+([+-])\z/
           cmd, name, dir = $1, $2, $3
           next TrovoBot::queue.push ["access denied", channel_id] unless "8" <= get_level[chat[:sender_id], channel_id, cmd]
           id = TrovoBot::name_to_id name
-          db.transaction{ |tr| tr["access.#{cmd}.#{channel_id}.#{id}"] = dir == ?+ ? 1 : 0 }
-          TrovoBot::queue.push ["#{name}'s new access level: #{get_level[id, channel_id, cmd]}", channel_id]
+          db.transaction{ |tr| tr["access.#{resolve[cmd]}.#{channel_id}.#{id}"] = dir == ?+ ? 1 : 0 }
+          TrovoBot::queue.push ["#{name}'s new \\#{$1} access level: #{get_level[id, channel_id, cmd]}", channel_id]
 
-        when /\A\\help\s+quote(\s|\z)/
+        when /\A\\#{re_help}\s+#{re_quote}(\s|\z)/
           TrovoBot::queue.push [
             "show random quote: \\quote; "\
             "show specific quote: \\quote <number>; "\
@@ -57,12 +70,12 @@ TrovoBot.start do |chat, channel_id|   # this is designed for a multichannel bot
             "delete quote (only for quote author, channel owner, and bot admin): \\quote del <number>",
             channel_id
           ]
-        when /\A\\q(?:uote)?\z/
+        when /\A\\#{re_quote}\z/
           i, quote = db.transaction(true) do |tr|
             tr.roots.grep(/\Aquote\.#{channel_id}\./).map{ |id| [id.split(?.).last, tr[id]] }
           end.select(&:last).sample
           TrovoBot::queue.push [quote ? "##{i}: #{quote[:text]}" : "no quotes yet, go ahead and use '\\quote add <text>' to add some!", channel_id]
-        when /\A\\q(?:uote)?\s+search\s+(\S.*?)\s*\z/
+        when /\A\\#{re_quote}\s+search\s+(\S.*?)\s*\z/
           text = $1
           found = db.transaction(true) do |tr|
             tr.roots.grep(/\Aquote\.#{channel_id}\./).map{ |id| [id.split(?.).last, tr[id]] }
@@ -75,11 +88,11 @@ TrovoBot.start do |chat, channel_id|   # this is designed for a multichannel bot
             end,
             channel_id
           ]
-        when /\A\\q(?:uote)?\s+(\d+)\z/
+        when /\A\\#{re_quote}\s+(\d+)\z/
           i = $1
           quote = db.transaction(true){ |tr| tr["quote.#{channel_id}.#{i}"] }
           TrovoBot::queue.push [quote ? "##{i}: #{quote[:text]}" : "quote ##{i} not found", channel_id]
-        when /\A\\q(?:uote)?\s+add\s+(\S.*?)\s*\z/
+        when /\A\\#{re_quote}\s+add\s+(\S.*?)\s*\z/
           text = $1
           next TrovoBot::queue.push ["access denied", channel_id] unless "1" <= get_level[chat[:sender_id], channel_id, "quote"]
           i = db.transaction do |tr|
@@ -88,7 +101,7 @@ TrovoBot.start do |chat, channel_id|   # this is designed for a multichannel bot
             end
           end
           TrovoBot::queue.push ["quote ##{i} added", channel_id]
-        when /\A\\q(?:uote)?\s+del\s+(\d+)\z/
+        when /\A\\#{re_quote}\s+del\s+(\d+)\z/
           i = $1
           next TrovoBot::queue.push ["access denied", channel_id] unless "1" <= get_level[chat[:sender_id], channel_id, "quote"]
           result = db.transaction do |tr|
@@ -107,7 +120,7 @@ TrovoBot.start do |chat, channel_id|   # this is designed for a multichannel bot
         # bet revert
         # bet cancel = finish + revert
 
-        when /\A\\help(\s|\z)/
+        when /\A\\#{re_help}(\s|\z)/
           TrovoBot::queue.push [
             "for help: \\help <command>; "\
             "available commands: help, access, quote",
--- examples/velik_the_bot/test.rb
+++ examples/velik_the_bot/test.rb
@@ -42,43 +42,43 @@ describe "" do
       ["bet", "participate", "initiate"],
     ].each do |cmd, l0, l1|
       it "\\access #{cmd} get" do
-        b [["\\access #{cmd}", "'s current access level: 8 (owner)"]]
-        b [["\\access #{cmd}", "'s current access level: 9 (admin)"]], "admin"
-        b [["\\access #{cmd}", "'s current access level: 0 (#{l0})"]], "someone"
+        b [["\\access #{cmd}", "'s current \\#{cmd} access level: 8 (owner)"]]
+        b [["\\access #{cmd}", "'s current \\#{cmd} access level: 9 (admin)"]], "admin"
+        b [["\\access #{cmd}", "'s current \\#{cmd} access level: 0 (#{l0})"]], "someone"
         [nil, "admin", "someone"].each do |who|
           b [
-            ["\\access #{cmd} owner", "owner's current access level: 8 (owner)"],
-            ["\\access #{cmd} admin", "admin's current access level: 9 (admin)"],
-            ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
+            ["\\access #{cmd} owner", "owner's current \\#{cmd} access level: 8 (owner)"],
+            ["\\access #{cmd} admin", "admin's current \\#{cmd} access level: 9 (admin)"],
+            ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
           ], *who
         end
       end
       it "\\access #{cmd} set" do
         File.write "db.yaml", YAML.dump({})
         b [
-          ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
-          ["\\access #{cmd} someone +", "someone's new access level: 1 (#{l1})"],
-          ["\\access #{cmd} someone", "someone's current access level: 1 (#{l1})"],
-          ["\\access #{cmd} someone -", "someone's new access level: 0 (#{l0})"],
-          ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone +", "someone's new \\#{cmd} access level: 1 (#{l1})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 1 (#{l1})"],
+          ["\\access #{cmd} someone -", "someone's new \\#{cmd} access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
         ]
         b [
-          ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
           ["\\access #{cmd} someone +", "access denied"],
-          ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
           ["\\access #{cmd} someone -", "access denied"],
         ], "someone"
         b [
-          ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
-          ["\\access #{cmd} someone +", "someone's new access level: 1 (#{l1})"],
-          ["\\access #{cmd} someone", "someone's current access level: 1 (#{l1})"],
-          ["\\access #{cmd} someone -", "someone's new access level: 0 (#{l0})"],
-          ["\\access #{cmd} someone", "someone's current access level: 0 (#{l0})"],
-          ["\\access #{cmd} someone +", "someone's new access level: 1 (#{l1})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone +", "someone's new \\#{cmd} access level: 1 (#{l1})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 1 (#{l1})"],
+          ["\\access #{cmd} someone -", "someone's new \\#{cmd} access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone", "someone's current \\#{cmd} access level: 0 (#{l0})"],
+          ["\\access #{cmd} someone +", "someone's new \\#{cmd} access level: 1 (#{l1})"],
         ], "admin"
         b [
           ["\\access #{cmd} someone -", "access denied"],
-          ["\\access #{cmd}", "'s current access level: 1 (#{l1})"],
+          ["\\access #{cmd}", "'s current \\#{cmd} access level: 1 (#{l1})"],
         ], "someone"
       end
     end
@@ -116,7 +116,7 @@ describe "" do
         ["\\quote 4", "quote #4 not found"],
         ["\\quote del 3", "access denied"],
       ], "someone"
-      b [["\\access quote someone +", "someone's new access level: 1 (add)"]]
+      b [["\\access quote someone +", "someone's new \\quote access level: 1 (add)"]]
       b [
         ["\\quote add hello world", "quote #4 added"],
         ["\\quote 4", "#4: hello world"],
@@ -125,7 +125,7 @@ describe "" do
         ["\\quote 4", "quote #4 not found"],
         ["\\quote add hello world", "quote #5 added"],
       ], "someone"
-      b [["\\access quote someone -", "someone's new access level: 0 (query)"]]
+      b [["\\access quote someone -", "someone's new \\quote access level: 0 (query)"]]
       b [
         ["\\quote add hello world", "access denied"],
         ["\\quote del 5", "access denied"],
